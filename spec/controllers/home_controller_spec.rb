076a4df6c1d8e21d98d7dbf0a45f183cd7cd2b53
sue445 <sue445@sue445.net>
2018-01-19 00:14:49 +0900

    :cop: rubocop --auto-correct --only Layout/EmptyLinesAroundBlockBody

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,10 +1,8 @@
 RSpec.describe HomeController, type: :controller do
-
   describe "GET #index" do
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
     end
   end
-
 end
