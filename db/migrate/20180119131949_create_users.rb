3947434510c3653f04742a2f626f2d21a5b1f0a4
sue445 <sue445@sue445.net>
2018-01-19 22:23:43 +0900

    Tweak migration

--- db/migrate/20180119131949_create_users.rb
+++ db/migrate/20180119131949_create_users.rb
@@ -1,15 +1,16 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.integer :account_id
-      t.integer :room_id
-      t.string :access_token
-      t.string :refresh_token
-      t.datetime :access_token_expires_at
-      t.datetime :refresh_token_expires_at
-      t.datetime :refresh_token_reminded_at
+      t.integer  :account_id,               null: false
+      t.integer  :room_id,                  null: false
+      t.string   :access_token,             null: false
+      t.string   :refresh_token,            null: false
+      t.datetime :access_token_expires_at,  null: false
+      t.datetime :refresh_token_expires_at, null: false
 
       t.timestamps
     end
+
+    add_index :users, :account_id, unique: true
   end
 end
