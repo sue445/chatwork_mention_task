6c1195f63abe2b7cf3d8699e35c2ccd7520c60ea
sue445 <sue445@sue445.net>
2018-01-21 17:38:16 +0900

    Enabled apply_to_host_groups

--- spec/support/contexts/time_stop.rb
+++ spec/support/contexts/time_stop.rb
@@ -1,11 +1,12 @@
-RSpec.configure do |config|
-  config.include ActiveSupport::Testing::TimeHelpers
-end
-
 RSpec.shared_context :time_stop, :time_stop do
   around do |example|
     travel_to(Time.current) do
       example.run
     end
   end
+end
+
+RSpec.configure do |config|
+  config.include ActiveSupport::Testing::TimeHelpers
+  config.include_context :time_stop, :time_stop
 end
