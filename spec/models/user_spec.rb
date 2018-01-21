d9acd4ac7b4601ad86b7cba2e266b79fa2507971
sue445 <sue445@sue445.net>
2018-01-22 01:01:23 +0900

    Impl User#with_retryable

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -58,4 +58,53 @@ RSpec.describe User, type: :model do
     its(:access_token_expires_at)  { should match_unixtime(1_510_504_991.seconds.from_now) }
     its(:refresh_token_expires_at) { should match_unixtime(User::REFRESH_TOKEN_EXPIRES_IN.from_now) }
   end
+
+  describe "#with_retryable", :time_stop do
+    subject do
+      user.with_retryable do
+        call_api
+      end
+    end
+
+    let(:user) { create(:user, access_token_expires_at: 1.day.ago) }
+    let(:access_token) { "new_access_token" }
+    let(:tokens) do
+      {
+        access_token:  "new_access_token",
+        token_type:    "Bearer",
+        expires_in:    "1800",
+        refresh_token: "refresh_token",
+        scope:         "users.all:read rooms.all:read_write contacts.all:read_write",
+      }
+    end
+
+    before do
+      @count = 0
+
+      allow(ChatWork::Token).to receive(:refresh_access_token) { tokens }
+    end
+
+    def call_api
+      @count += 1
+
+      if @count == 1 # rubocop:disable Style/GuardClause
+        raise ChatWork::AuthenticateError.new(
+          'Bearer error="invalid_token", error_description="The access token expired"',
+          401,
+          ["Invalid API Token"],
+          "invalid_token",
+          "The access token expired",
+        )
+      end
+    end
+
+    it { expect { subject }.to change { user.access_token }.to(access_token) }
+    it { expect { subject }.to change { user.access_token_expires_at }.to(30.minutes.from_now) }
+
+    it "call ChatWork::Token.refresh_access_token once" do
+      subject
+
+      expect(ChatWork::Token).to have_received(:refresh_access_token).once
+    end
+  end
 end
