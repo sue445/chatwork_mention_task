0c386f946dada1b4e9f74618aab50c4721030a92
sue445 <sue445@sue445.net>
2018-01-21 19:48:28 +0900

    Fixed. can not connect to memcached on production

--- config/initializers/cache_store.rb
+++ config/initializers/cache_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.cache_store = :mem_cache_store, Global.memcached.servers, { namespace: Rails.env }
+Rails.application.config.cache_store = :mem_cache_store, Global.memcached.servers, Global.memcached.options.to_hash.symbolize_keys
