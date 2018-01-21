29447ed2d6f8f53e9b6602f1ef12e9a7ee8cad8c
sue445 <sue445@sue445.net>
2018-01-21 20:03:26 +0900

    Use ActionDispatch::Session::CacheStore

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,1 @@
-params = Global.memcached.options.to_hash.symbolize_keys.reverse_merge(
-  key: "_chatwork_mention_task_session",
-  memcache_server: Global.memcached.servers,
-  namespace: Rails.env,
-)
-
-Rails.application.config.session_store :mem_cache_store, params
+Rails.application.config.session_store ActionDispatch::Session::CacheStore, expire_after: Global.memcached.session.expire_after
