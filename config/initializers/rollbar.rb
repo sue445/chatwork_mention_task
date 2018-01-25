fb996051422164a52fa154fb113980ae508f7e84
sue445 <sue445@sue445.net>
2018-01-26 00:17:44 +0900

    Send User#name to rollbar

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -11,7 +11,7 @@ Rollbar.configure do |config|
   # `username`, and `email` methods to fetch those properties. To customize:
   # config.person_method = "my_current_user"
   # config.person_id_method = "my_id"
-  # config.person_username_method = "my_username"
+  config.person_username_method = "name"
   # config.person_email_method = "my_email"
 
   # If you want to attach custom data to all exception and message reports,
