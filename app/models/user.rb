ad47881eb4253aa4656fc6a543e01a0a71b012de
sue445 <sue445@sue445.net>
2018-01-28 23:01:27 +0900

    Refactor: extract to ApiModule

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,8 @@
 require "chatwork/chatwork_error"
 
 class User < ApplicationRecord
+  include User::ApiModule
+
   # c.f. http://download.chatwork.com/ChatWork_API_Documentation.pdf
   REFRESH_TOKEN_EXPIRES_IN = 14.days
 
@@ -53,61 +55,4 @@ class User < ApplicationRecord
 
     user
   end
-
-  def rooms
-    all_rooms = with_retryable do
-      ChatWork::Room.get
-    end
-
-    all_rooms.select { |room| ["my", "group"].include?(room.type) && ["admin", "member"].include?(room.role) }.sort_by(&:name)
-  end
-
-  def room_name
-    room = with_retryable do
-      ChatWork::Room.find(room_id: room_id)
-    end
-
-    return "My Chat" if room.type == "my"
-    room.name
-  end
-
-  def create_task(room_id:, body:, to_ids:, limit_at: nil)
-    with_retryable do
-      ChatWork::Task.create(room_id: room_id, body: body, to_ids: to_ids, limit: limit_at&.to_i)
-    end
-  end
-
-  def create_my_task(body, limit_at: nil)
-    create_task(room_id: room_id, body: body, to_ids: account_id, limit_at: limit_at)
-  end
-
-  def with_retryable
-    set_token
-
-    yield
-  rescue ChatWork::AuthenticateError => error
-    retry_count ||= 0
-    retry_count += 1
-
-    if retry_count == 1 && error.error == "invalid_token"
-      refresh_access_token
-      retry
-    end
-
-    raise
-  end
-
-  private
-
-    def set_token
-      ChatWork.access_token = access_token
-    end
-
-    def refresh_access_token
-      tokens = ChatWork::Token.refresh_access_token(refresh_token)
-      self.access_token = tokens[:access_token]
-      self.refresh_token = tokens[:refresh_token]
-      self.access_token_expires_at = tokens[:expires_in].to_i.seconds.from_now
-      save!
-    end
 end
