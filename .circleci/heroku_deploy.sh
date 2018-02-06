65ff766de5dfbab61dc79a0491bda90559686c1e
sue445 <sue445@users.noreply.github.com>
2018-02-06 23:32:28 +0900

    Disable self deploy check
    For save heroku instance hour

--- .circleci/heroku_deploy.sh
+++ .circleci/heroku_deploy.sh
@@ -6,4 +6,4 @@ git push git@heroku.com:$HEROKU_APP_NAME.git $CIRCLE_SHA1:refs/heads/master
 
 heroku run rake db:migrate --app $HEROKU_APP_NAME
 
-curl -s "https://${HEROKU_APP_NAME}.herokuapp.com/ops/heartbeat" | grep "heartbeat:ok"
+# curl -s "https://${HEROKU_APP_NAME}.herokuapp.com/ops/heartbeat" | grep "heartbeat:ok"
