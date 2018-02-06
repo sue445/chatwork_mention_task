6581cc580182edb631a716471f5f6622e42250e2
sue445 <sue445@sue445.net>
2018-02-06 10:13:05 +0900

    Use restore_bundled_with

--- .circleci/bundle_install.sh
+++ .circleci/bundle_install.sh
@@ -2,3 +2,7 @@
 
 bundle install --jobs=4 --retry=3 --path vendor/bundle
 bundle clean
+
+# Resolve bundler version difference between Gemfile.lock and pre-installed in CI
+gem install restore_bundled_with --no-document
+restore-bundled-with
