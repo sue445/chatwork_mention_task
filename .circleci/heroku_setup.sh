5a92407fe0ec8ccfefa8321bad493eae64e9f29f
sue445 <sue445@sue445.net>
2018-02-11 10:12:56 +0900

    Revert "Tweak deploy script"
    This reverts commit 792630277e44256d246939e228cb393fd1a367ec.

--- .circleci/heroku_setup.sh
+++ .circleci/heroku_setup.sh
@@ -3,9 +3,6 @@ cat > ~/.netrc << EOF
 machine api.heroku.com
   login $HEROKU_LOGIN
   password $HEROKU_API_KEY
-machine git.heroku.com
-  login $HEROKU_LOGIN
-  password $HEROKU_API_KEY
 EOF
 
 mkdir ~/.ssh/
