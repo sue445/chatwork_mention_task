792630277e44256d246939e228cb393fd1a367ec
sue445 <sue445@users.noreply.github.com>
2018-02-11 10:04:49 +0900

    Tweak deploy script
    c.f. https://circleci.com/docs/2.0/project-walkthrough/

--- .circleci/heroku_setup.sh
+++ .circleci/heroku_setup.sh
@@ -3,6 +3,9 @@ cat > ~/.netrc << EOF
 machine api.heroku.com
   login $HEROKU_LOGIN
   password $HEROKU_API_KEY
+machine git.heroku.com
+  login $HEROKU_LOGIN
+  password $HEROKU_API_KEY
 EOF
 
 mkdir ~/.ssh/
