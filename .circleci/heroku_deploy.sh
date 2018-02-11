e65d2b4c7ae0befff2b54fe62376ac84958762ec
sue445 <sue445@users.noreply.github.com>
2018-02-11 10:23:24 +0900

    sleep

--- .circleci/heroku_deploy.sh
+++ .circleci/heroku_deploy.sh
@@ -2,6 +2,8 @@
 
 heroku config:add BUNDLE_WITHOUT="test:development" --app $HEROKU_APP_NAME
 
+sleep 10
+
 git push git@heroku.com:$HEROKU_APP_NAME.git $CIRCLE_SHA1:refs/heads/master
 
 heroku run rake db:migrate --app $HEROKU_APP_NAME
