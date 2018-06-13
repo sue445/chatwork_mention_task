7ae3a215a88af57868e5d8f2c2c86379874ecc65
sue445 <sue445@sue445.net>
2018-06-13 18:31:18 +0900

    Add contact link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,10 @@ module ApplicationHelper
       t("app.title")
     end
   end
+
+  def contact_link
+    return nil if ENV["CONTACT_LINK"].blank? || !signed_in?
+
+    ENV["CONTACT_LINK"].gsub("${account_id}", current_user.account_id.to_s).gsub("${name}", current_user.name)
+  end
 end
